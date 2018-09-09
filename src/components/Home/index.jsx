b51496466887f11773e4c664124398ce28c08091
IMRON REVIADY <imronreviady2@gmail.com>
2018-09-09 18:42:55 +0700

    create home components

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+
+const Home = () => {
+	return (
+		<Fragment>
+			<div className="fh5co-narrow-content">
+				<div className="row">
+					<div className="col-md-5">
+						<h2 className="fh5co-heading animate-box" data-animate-effect="fadeInLeft">I Create Web And Mobile Apps</h2>
+						<p className="fh5co-lead animate-box" data-animate-effect="fadeInLeft">I am <a>Imron Reviady</a> a <a>Web And Mobile Apps Developer</a> From Depok, Indonesia.</p>
+					</div>
+					<div className="col-md-6 col-md-push-1 animate-box" data-animate-effect="fadeInLeft">
+						<img src="assets/images/img_1.jpg" alt="Free HTML5 Bootstrap Template" className="img-responsive" /> 
+					</div>
+				</div>
+				
+			</div>
+		</Fragment>
+	);
+};
+
+export default Home;
