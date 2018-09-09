0e26d313b196aa90bf7341fb2a1ee771607ececc
IMRON REVIADY <imronreviady2@gmail.com>
2018-09-09 17:48:09 +0700

    first commit

---
+++ src/components/LeftMenu/index.jsx
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+
+const LeftMenu = () => {
+	return (
+		<Fragment>
+			<a href="#" className="js-fh5co-nav-toggle fh5co-nav-toggle"><i /></a>
+			<aside id="fh5co-aside" role="complementary" className="border js-fullheight">
+				<h1 id="fh5co-logo"><a href="index.html"><img src="https://avatars0.githubusercontent.com/u/19647405?s=112&v=4" style={{borderRadius: '50%'}} alt="Free HTML5 Bootstrap Website Template" /></a></h1>
+				<nav id="fh5co-main-menu" role="navigation">
+					<ul>
+						<li className="fh5co-active"><a href="index.html">Home</a></li>
+						<li><a href="portfolio.html">Portfolio</a></li>
+						<li><a href="about.html">About</a></li>
+						<li><a href="contact.html">Contact</a></li>
+					</ul>
+				</nav>
+				<div className="fh5co-footer">
+					<ul>
+						<li><a href="#"><i className="icon-facebook" /></a></li>
+						<li><a href="#"><i className="icon-twitter" /></a></li>
+						<li><a href="#"><i className="icon-instagram" /></a></li>
+						<li><a href="#"><i className="icon-linkedin" /></a></li>
+					</ul>
+				</div>
+			</aside>
+		</Fragment>
+	);
+};
+
+export default LeftMenu;
