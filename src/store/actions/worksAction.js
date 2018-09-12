6f682a9b3af88baf9c62421555199934a578e897
IMRON REVIADY <imronreviady2@gmail.com>
2018-09-12 16:43:00 +0700

    Done get works list from firebase

--- src/store/actions/worksAction.js
+++ src/store/actions/worksAction.js
@@ -1,6 +1,17 @@
 import { GET_WORKS, SAVE_WORK } from '../actionTypes';
 import { worksDb } from '../../firebase';
 
+export function getWorks() {
+	return dispatch => {
+		worksDb.on('value', snapshot => {
+			dispatch({
+				type: GET_WORKS,
+				payload: snapshot.val()
+			});
+		});
+	}
+}
+
 export function saveWork(work) {
 	console.log(work);
 	return dispatch => worksDb.push(work);
