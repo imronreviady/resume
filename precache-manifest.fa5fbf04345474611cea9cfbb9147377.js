2b44b65d9eaf2273ede3c8e9c649cbce43a96f90
IMRON REVIADY <imronreviady2@gmail.com>
2019-07-22 15:00:22 +0700

    Updates

---
+++ precache-manifest.fa5fbf04345474611cea9cfbb9147377.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cc630a0e2e81ff21e586",
+    "url": "/static/js/main.cc630a0e.chunk.js"
+  },
+  {
+    "revision": "441e2a42c1111ab5bcf0",
+    "url": "/static/js/1.441e2a42.chunk.js"
+  },
+  {
+    "revision": "441e2a42c1111ab5bcf0",
+    "url": "/static/css/1.8378bac9.chunk.css"
+  },
+  {
+    "revision": "03ab63c291953cf69b2a6a2ff351d93d",
+    "url": "/index.html"
+  }
+];
